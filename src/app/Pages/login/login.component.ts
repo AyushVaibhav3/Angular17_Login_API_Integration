14093bd5c73974a7fea9fd4dbc041127b32c713b
Ayush Vaibhav <ayush.vaibhav3@cognizant.com>
2024-10-16 15:48:15 +0530

    Implemented Login Function

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,14 +1,50 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, Renderer2 } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-constructor(private render:Renderer2){
-  this.render.setStyle(document.body, 'background-color', 'white');
+  // constructor(private render:Renderer2){
+  //   this.render.setStyle(document.body, 'background-color', 'white');
+  // }
+  loginObj: Login;
+
+  constructor(private http: HttpClient) {
+    this.loginObj = new Login();
+
+  }
+
+  onLogin(){
+    // alert('Login Clicked');
+    debugger;
+    this.http.post('https://freeapi.miniprojectideas.com/api/User/Login',this.loginObj).subscribe((res:any)=>{
+    
+    if(res.result){
+        alert('Login Success');
+      }
+      else{
+        alert(res.message);
+      }
+    })
+  }
+
 }
+
+export class Login {
+
+  EmailId: string;
+  Password: string;
+
+  constructor() {
+    this.EmailId = '';
+    this.Password = '';
+  }
+
 }
+// 
