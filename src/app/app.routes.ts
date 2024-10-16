85e1058e79373893b244052d3a5e21fa6b1dc455
Ayush Vaibhav <ayush.vaibhav3@cognizant.com>
2024-10-16 12:12:14 +0530

    Implemented Routings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Pages/login/login.component';
+import { DashboardComponent } from './Pages/dashboard/dashboard.component';
+import { LayoutComponent } from './Pages/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'', redirectTo:'login',pathMatch:'full'
+    },
+    {
+        path:'login',component:LoginComponent
+    },
+    
+    {
+        path:'',component:LayoutComponent,
+        children:[
+            {
+                path:'dashboard',component:DashboardComponent
+            }
+        ]
+    }
+    
+];
